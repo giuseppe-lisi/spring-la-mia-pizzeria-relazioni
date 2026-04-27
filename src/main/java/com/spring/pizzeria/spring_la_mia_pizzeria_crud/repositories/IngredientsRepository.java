ecc9ed075ea4fef651a58df459f7880c1ac1de3e
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-27 15:04:01 +0200

    mancavano java generics

--- src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/repositories/IngredientsRepository.java
+++ src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/repositories/IngredientsRepository.java
@@ -2,6 +2,8 @@ package com.spring.pizzeria.spring_la_mia_pizzeria_crud.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientsRepository extends JpaRepository {
+import com.spring.pizzeria.spring_la_mia_pizzeria_crud.models.Ingredient;
+
+public interface IngredientsRepository extends JpaRepository<Ingredient, Integer> {
     
 }
