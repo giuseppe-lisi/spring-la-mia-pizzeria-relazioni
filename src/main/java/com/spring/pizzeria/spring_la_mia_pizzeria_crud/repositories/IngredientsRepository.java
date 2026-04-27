14d109f0bfea01176a1000a05e2d8cdba633b887
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-27 15:02:21 +0200

    creata repo ingredienti

---
+++ src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/repositories/IngredientsRepository.java
@@ -0,0 +1,7 @@
+package com.spring.pizzeria.spring_la_mia_pizzeria_crud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientsRepository extends JpaRepository {
+    
+}
